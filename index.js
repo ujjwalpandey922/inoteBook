ed6c45876787047292159705c0706579d82efd3b
ujjwalpandey922 <104821808+ujjwalpandey922@users.noreply.github.com>
2022-08-16 15:42:50 +0530

    trying 13

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 //heroku deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("frontend/build")));
+  app.use(express.static("frontend/build"));
   const path = reqire("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
